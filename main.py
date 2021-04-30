e635fae69e430481fe9f4c31f7247b378d9e6403
avada420 <83414466+avada420@users.noreply.github.com>
2021-04-30 17:46:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ class Game:
         self.comb_plus_throw = {(1, 2, 3, 4, 5, 6): 1000, (1, 1, 2, 2, 3, 3): 750}
         self.combinations = {(1): 100, (5): 50, }
 
-        
-        
     def finish_turn(self):
         self.turn *= -1
 
