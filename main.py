554ef16e13220c670b3d877df08cf01d1c4c168e
grogeru <78545073+grogeru@users.noreply.github.com>
2021-04-30 17:42:31 +0500

    ну ибо нефиг

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from random import choice
+
+
 class Game:
     def __init__(self):
         self.turn = choice((1, -1))
         self.player1_score = 0
         self.player2_score = 0
         self.comb_plus_throw = {(1, 2, 3, 4, 5, 6): 1000, (1, 1, 2, 2, 3, 3): 750}
-        self.combinations = {(1): 100, (5): 50, }
+        self.combinations = {(1, 1, 1, 1, 1, 1): 6000, (2, 2, 2, 2, 2, 2): 1600, (3, 3, 3, 3, 3, 3): 2400, (4, 4, 4, 4, 4, 4): 3200, (5, 5, 5, 5, 5, 5): 4000, (6, 6, 6, 6, 6, 6): 4800,}
 
     def finish_turn(self):
         self.turn *= -1
@@ -12,3 +15,4 @@ class Game:
     def put_away(self):
         def count_(self):
             pass
+
