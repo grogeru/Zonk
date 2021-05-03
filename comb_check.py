21dd11cf48c56a92f8a86aad52a27067c8c1dbbb
grogeru <78545073+grogeru@users.noreply.github.com>
2021-05-03 18:49:41 +0500

    Update comb_check.py

--- comb_check.py
+++ comb_check.py
@@ -2,11 +2,10 @@
 from collections import Counter
 
 data_check_all_of_a_kind = {1: 6000, 2: 1600, 3: 2400, 4: 3200, 5: 4000, 6: 4800}
-data_check_for_5 = {}
+data_check_for_5 = {1: 3000, 2: 800, 3: 1200, 4: 1600, 5: 2000, 6: 2400}
 data_check_for_4 = {1: 2000, 2: 400, 3: 600, 4: 800, 5: 1000, 6: 1200}
-data_check_for_3 = {}
-
-
+data_check_for_3 = {1: 1000, 2: 200, 3: 300, 4: 400, 5: 500, 6: 600}
+data_check_for_1 = {1: 100, 5: 50}
 
 
 def check_1_2_3_4_5_6(lst):
@@ -22,20 +21,10 @@ def check_all_of_a_kind(lst): # 1
         return data_check_all_of_a_kind[lst[0]]
 
 
-def check_for_5(lst):
-    if len(set(lst)) == 2 and (lst[0] != lst[1] or lst[4] != lst[5]): # 3
-        if lst.count(1) == 5:
-            return 3000
-        elif lst.count(2) == 5:
-            return 800
-        elif lst.count(3) == 5:
-            return 800
-        elif lst.count(4) == 5:
-            return 1600
-        elif lst.count(5) == 5:
-           return 2000
-        else:
-            return 2400
+def check_for_5(count):
+    for num in count:
+        if count[num] == 5:
+            return data_check_for_5[num]
 
 def check_for_4(count): # count = Counter(lst) # 6
     for num in count:
@@ -45,19 +34,15 @@ def check_for_4(count): # count = Counter(lst) # 6
 def check_for_3(count): # 6
     for num in count:  # if len(set(lst)) == 2 and lst[2] != lst[3]:
         if count[num] == 3:
-            return
+            return data_check_for_3[num]
 
+def che_for_1(count):
+    for num in count:
+        if count[num] == 1:
+            if num == 1:
+                return 100
+            elif num == 5:
+                return 50
+            else:
+                return None
 
-
-
-
-
-
-
-        #self.combinations = {
-
-                             #   (1, 1, 1, 1): 2000, (6, 6, 6, 6): 1200, (5, 5, 5, 5): 1000, (4, 4, 4, 4): 800, (3, 3, 3, 3): 600, (2, 2, 2, 2): 400,
-
-
-                             #(1, 1, 1): 1000, (2, 2, 2): 200, (3, 3, 3): 300, (4, 4, 4): 400, (5, 5, 5): 500,
-                             #(6, 6, 6): 600, (1): 100, (5): 50}
